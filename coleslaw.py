10f6dbe3d84a0c04ab98d62aea08b84aadfc821d
Zayne64 <11258171+Zayne64@users.noreply.github.com>
2020-05-16 14:26:16 +0100

    ColeSlaw loader

---
+++ coleslaw.py
@@ -0,0 +1,10 @@
+import csArguments
+import csVariables
+import csStrings
+import csFunctional
+import csMiscellaneous
+import csTime
+
+class ColeSlaw:
+  csVERSION = '0.5-ColeSlaw.a1'
+  csREPO_URL = 'https://github.com/Zayne64/ColeSlaw'
